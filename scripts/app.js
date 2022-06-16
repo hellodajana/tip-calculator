dbabfea35faff5f7b0439fb8439c2517c94765a0
hellodajana <dajanasmolyakov@gmail.com>
2022-06-16 14:15:27 +0200

    Started calculator function

--- scripts/app.js
+++ scripts/app.js
@@ -5,11 +5,12 @@ inputFields.forEach((element) => {
   element.addEventListener("input", handleInput);
 });
 
+const [billInput, peopleInput] = [
+  parseFloat(document.getElementById("bill-input").value),
+  parseFloat(document.getElementById("people-input").value),
+];
+
 function handleInput() {
-  const [billInput, peopleInput] = [
-    parseFloat(document.getElementById("bill-input").value),
-    parseFloat(document.getElementById("people-input").value),
-  ];
   //   console.log(billInput, peopleInput);
   // if zero, add red border and display:block error
   if (billInput < 1) {
@@ -18,9 +19,19 @@ function handleInput() {
   if (peopleInput < 1) {
     document.getElementById("people-error").style.display = "block";
   }
+  tipCalc(billInput, peopleInput);
 }
 
 // calculate tip
+
+function tipCalc(bill, people) {
+  const tipValue = 20;
+  let tipAmount = (bill * tipValue) / people;
+  let totalAmount = (bill + tipAmount) / people;
+  document.getElementById("tip-amount").innerHTML = "$" + tipAmount.toFixed(2);
+  document.getElementById("total-amount").innerHTML =
+    "$" + totalAmount.toFixed(2);
+}
 
 // save amount as innerHTML
 
