2cd0184a0d372e32d4c9965584e6bdd049918ee0
hellodajana <dajanasmolyakov@gmail.com>
2022-06-17 13:39:32 +0200

    Reset button

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,21 @@
 const inputFields = document.querySelectorAll(".input-amount");
+
 inputFields.forEach((element) => {
   element.addEventListener("input", (e) => {
     // save input number
-    const [billInput, peopleInput] = [
+    const [billInput, peopleInput, tipInput] = [
       parseFloat(document.getElementById("bill-input").value),
       parseFloat(document.getElementById("people-input").value),
+      parseFloat(document.getElementById("tip-custom").value),
     ];
+    console.log(tipInput);
     tipCalc(billInput, peopleInput);
     handleError(billInput, peopleInput);
   });
 });
+
+// reset button
+document.getElementById("reset-button").addEventListener("click", resetButton);
 
 function handleError(bill, people) {
   // if zero, add red border and display:block error
@@ -25,12 +31,21 @@ function handleError(bill, people) {
   }
 }
 
+// tip buttons
+document.querySelectorAll(".tip-buttons").forEach((element) => {
+  element.addEventListener("click", function tipButton(event) {
+    console.log(event.target.innerText);
+  });
+});
+
 function tipCalc(bill, people) {
   // calculate tip
   const tipValue = 20;
   let tipAmount = bill * (tipValue / 100);
-  let tipAmountPerPerson = tipAmount / people;
-  let totalAmount = (tipAmount + bill) / people;
+  let [tipAmountPerPerson, totalAmount] = [
+    tipAmount / people,
+    (tipAmount + bill) / people,
+  ];
   // save amount as innerHTML
   document.getElementById("tip-amount").innerHTML =
     "$" + tipAmountPerPerson.toFixed(2);
@@ -38,4 +53,8 @@ function tipCalc(bill, people) {
     "$" + totalAmount.toFixed(2);
 }
 
-//reset button
+function resetButton() {
+  inputFields.forEach((element) => (element.value = ""));
+  document.getElementById("tip-amount").innerHTML = "$0.00";
+  document.getElementById("total-amount").innerHTML = "$0.00";
+}
