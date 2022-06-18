3c69e558454cfe5fd75f70717372af7e5d2e384c
hellodajana <dajanasmolyakov@gmail.com>
2022-06-18 22:46:59 +0200

    Tip function updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,43 @@
 const inputFields = document.querySelectorAll(".input-amount");
 
+const [billInput, peopleInput, tipInput] = [
+  parseFloat(document.getElementById("bill-input").value),
+  parseFloat(document.getElementById("people-input").value),
+  parseFloat(document.getElementById("tip-custom").value),
+];
+
 inputFields.forEach((element) => {
   element.addEventListener("input", (e) => {
     // save input number
-    const [billInput, peopleInput, tipInput] = [
-      parseFloat(document.getElementById("bill-input").value),
-      parseFloat(document.getElementById("people-input").value),
-      parseFloat(document.getElementById("tip-custom").value),
-    ];
-    console.log(tipInput);
-    tipCalc(billInput, peopleInput);
+    tipCalc(billInput, peopleInput, tipInput);
     handleError(billInput, peopleInput);
   });
 });
 
-// reset button
-document.getElementById("reset-button").addEventListener("click", resetButton);
+// tip buttons
+document.querySelectorAll(".tip-buttons").forEach((element) => {
+  element.addEventListener("click", function tipButton(event) {
+    const tip = parseFloat(event.target.innerText.replace("%", ""));
+    tipCalc(billInput, peopleInput, tip);
+    console.log(tip);
+  });
+});
 
+// calculate tip
+function tipCalc(bill, people, tip) {
+  const tipAmount = bill * (tip / 100);
+  const [tipAmountPerPerson, totalAmount] = [
+    tipAmount / people,
+    (tipAmount + bill) / people,
+  ];
+  // save amount as innerHTML
+  document.getElementById("tip-amount").innerHTML =
+    "$" + tipAmountPerPerson.toFixed(2);
+  document.getElementById("total-amount").innerHTML =
+    "$" + totalAmount.toFixed(2);
+}
+
+// if error
 function handleError(bill, people) {
   // if zero, add red border and display:block error
   if (bill < 1) {
@@ -31,30 +52,15 @@ function handleError(bill, people) {
   }
 }
 
-// tip buttons
-document.querySelectorAll(".tip-buttons").forEach((element) => {
-  element.addEventListener("click", function tipButton(event) {
-    console.log(event.target.innerText);
-  });
-});
+// reset button
+document.getElementById("reset-button").addEventListener("click", resetButton);
 
-function tipCalc(bill, people) {
-  // calculate tip
-  const tipValue = 20;
-  let tipAmount = bill * (tipValue / 100);
-  let [tipAmountPerPerson, totalAmount] = [
-    tipAmount / people,
-    (tipAmount + bill) / people,
-  ];
-  // save amount as innerHTML
-  document.getElementById("tip-amount").innerHTML =
-    "$" + tipAmountPerPerson.toFixed(2);
-  document.getElementById("total-amount").innerHTML =
-    "$" + totalAmount.toFixed(2);
-}
-
+// reset button
 function resetButton() {
   inputFields.forEach((element) => (element.value = ""));
   document.getElementById("tip-amount").innerHTML = "$0.00";
   document.getElementById("total-amount").innerHTML = "$0.00";
+  document.getElementById("tip-custom").innerHTML = "Custom";
 }
+
+// print price
