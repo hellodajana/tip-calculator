9c98ee80b000aac7e7537fc6c99df9f44874904e
hellodajana <dajanasmolyakov@gmail.com>
2022-06-23 22:18:36 +0200

    Added function printAmount

--- scripts/app.js
+++ scripts/app.js
@@ -1,79 +1,80 @@
 const inputFields = document.querySelectorAll(".input-amount");
 const resetBtn = document.getElementById("reset-button");
 const tipButton = document.querySelectorAll(".tip-buttons");
+const tipInput = document.getElementById("tip-custom");
 
-const [billInput, peopleInput, tipInput] = [
+const [billInput, peopleInput] = [
   parseFloat(document.getElementById("bill-input").value),
   parseFloat(document.getElementById("people-input").value),
-  parseFloat(document.getElementById("tip-custom").value),
 ];
 
-// inputFields.forEach((element) => {
-//   element.addEventListener("input", (e) => {
-//     // save input number
-//     tipCalc(billInput, peopleInput, tipInput);
-//     handleError(billInput, peopleInput);
-//   });
-// });
-
+// event listeners
 function registerEventListener(element, event, callback) {
   return element.addEventListener(event, (e) => {
     callback(e);
   });
 }
-
-registerEventListener(inputFields, "input", tipCalc);
-registerEventListener(inputFields, "input", handleError);
+registerEventListener(tipInput, "input", getCustomTip);
 registerEventListener(resetBtn, "click", resetButton);
 
-// tip buttons
-function getTip() {}
-
 tipButton.forEach((element) => {
-  element.addEventListener("click", function tipButton(event) {
-    const tip = parseFloat(event.target.innerText.replace("%", ""));
-    tipCalc(billInput, peopleInput, tip);
-    console.log(tip);
-  });
+  registerEventListener(element, "click", getButtonTip);
 });
 
-// calculate tip
-function tipCalc(bill, people, callback) {
-  const tipAmount = bill * (callback / 100);
+inputFields.forEach((element) => {
+  registerEventListener(element, "input", handleError);
+});
+
+// functions
+
+const bill = function getBill() {};
+const people = function getPeople() {};
+
+function getCustomTip() {
+  const tip = parseFloat(tipInput.value / 100);
+  tipCalc(billInput, peopleInput, tip);
+}
+
+function getButtonTip(event) {
+  tipButton.forEach((element) => {
+    const tip = parseFloat(event.target.innerText.replace("%", ""));
+    tipCalc(bill, peopleInput, tip);
+  });
+}
+
+function tipCalc(bill, people, tip) {
+  const tipAmount = bill * (tip / 100);
   const [tipAmountPerPerson, totalAmount] = [
     tipAmount / people,
     (tipAmount + bill) / people,
   ];
-  // save amount as innerHTML
-  document.getElementById("tip-amount").innerHTML =
-    "$" + tipAmountPerPerson.toFixed(2);
-  document.getElementById("total-amount").innerHTML =
-    "$" + totalAmount.toFixed(2);
+  printAmount(tipAmountPerPerson, "tip-amount");
+  printAmount(totalAmount, "total-amount");
 }
 
-// if error
+function printAmount(amount, element) {
+  if (amount === "" || amount === NaN || amount === "undefined") {
+    return 0;
+  } else {
+    document.getElementById(element).innerHTML = "$" + amount.toFixed(2);
+  }
+}
+
 function handleError(bill, people) {
-  // if zero, add red border and display:block error
   const billError = document.getElementById("bill-error");
   const peopleError = document.getElementById("people-error");
-  if (bill < 1) {
-    billError.style.display = "block";
-  } else {
-    billError.style.display = "none";
-  }
-  if (people < 1) {
-    peopleError.style.display = "block";
-  } else {
-    peopleError.style.display = "none";
-  }
+  bill < 1
+    ? (billError.style.display = "block")
+    : (billError.style.display = "none");
+
+  people < 1
+    ? (peopleError.style.display = "block")
+    : (peopleError.style.display = "none");
 }
 
-// reset button
 function resetButton() {
   inputFields.forEach((element) => (element.value = ""));
   document.getElementById("tip-amount").innerHTML = "$0.00";
   document.getElementById("total-amount").innerHTML = "$0.00";
   document.getElementById("tip-custom").innerHTML = "Custom";
 }
-
-// print price
